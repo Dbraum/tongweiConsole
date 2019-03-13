ac5ba818a75ef852f0ee0728c11efa8a64332adf
kun <kun@MacBook-Pro-2.local>
2019-03-13 14:45:46 +0800

    add tongwei page

---
+++ src/pages/tongwei.vue
@@ -0,0 +1,103 @@
+<template>
+  <el-data-table v-bind="tableConfig"></el-data-table>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tableConfig: {
+        firstPage: 1,
+        dataPath: 'payload.content',
+        totalPath: 'payload.totalElements',
+        hasDialog: true,
+        hasPagination: true,
+        hasOperation: true,
+        isTree: false,
+        url: '/user/api/v1/products',
+        hasNew: true,
+        hasEdit: true,
+        hasView: false,
+        hasDelete: true,
+        single: false,
+        extraButtons: [],
+        columns: [
+          {
+            type: 'selection'
+          },
+          {
+            prop: 'id',
+            label: 'id'
+          },
+          {
+            prop: 'name',
+            label: '水果'
+          },
+          {
+            prop: 'price',
+            label: '价格'
+          }
+        ],
+        searchForm: [
+          {
+            $type: 'input',
+            $id: 'name',
+            label: '水果',
+            $el: {
+              placeholder: '请输入'
+            }
+          },
+          {
+            $type: 'input',
+            $id: 'price',
+            label: '价格',
+            $el: {
+              placeholder: '请输入'
+            }
+          }
+        ],
+        form: [
+          {
+            $type: 'input',
+            $id: 'name',
+            label: '水果',
+            $el: {
+              placeholder: '请输入'
+            },
+            rules: [
+              {
+                required: true,
+                message: '请输入水果',
+                trigger: 'blur'
+              }
+            ]
+          },
+          {
+            $type: 'input',
+            $id: 'price',
+            label: '价格',
+            $el: {
+              placeholder: '请输入'
+            },
+            rules: [
+              {
+                required: true,
+                message: '请输入价格',
+                trigger: 'blur'
+              }
+            ]
+          }
+        ]
+      }
+    }
+  },
+  methods: {}
+}
+</script>
+<style lang="less">
+.index {
+  .home-img {
+    width: 100%;
+  }
+}
+</style>
